90fb9a0176853eb159c36076f60a44583223e4b4
Isaac <isaacmactan@gmail.com>
2022-06-08 14:34:33 +1000

    Update MakeMask.py
    Iterates through the different directories for training and testing images, finds the corresponding annotation file and creates a mask

--- Software/MakeMask.py
+++ Software/MakeMask.py
@@ -6,9 +6,10 @@ import os
 from PIL import Image, ImageDraw
 
 
-dir = str("../Dataset/annotations/")
+annotationDir = str("../Dataset/annotations/")
+imageDir = str("../Dataset/images/train/")
 
-def extract_boxes(filename):
+def extract_info(filename):
     # load and parse the file
     tree = ET.parse(filename)
     # get the root of the document
@@ -32,19 +33,43 @@ def extract_boxes(filename):
     print("Filename ", annotationNum[0])
     return annotationNum[0], boxes, width, height
 
-for file in os.scandir(dir):
-    filename = os.fsdecode(file)
-    if filename.endswith(".xml"):
-        annotationNum, boxes, width, height = extract_boxes(filename)
+for image in os.scandir(imageDir):
+    imageName = os.fsdecode(image)
+    if imageName.endswith(".jpg"):
+        base = os.path.basename(image)
+        split = os.path.splitext(base)
+        imageNum = str(split[0])
+        # print(imageNum)
+        annotationNum, boxes, width, height = extract_info(annotationDir + imageNum +".xml")
         # creating new Image object
-        img = Image.new('RGB',(width,height),0)
+        img = Image.new('P',(width,height),'#00ff00')
         img1 = ImageDraw.Draw(img)
+        # img1.rectangle((0,0,width,height), fill = 'white', outline='white')
+        # img1.rectangle((0,0,width,height), fill = 'blue', outline='blue')
         for b in range(len(boxes)):
         # create rectangle image
-            img1.rectangle((boxes[b]), fill ='white',outline='white')
+            img1.rectangle((boxes[b]), fill ='red',outline='red')
         # img.show()
-        mask = '../Dataset/masks/' + annotationNum + '.png'
+        mask = '../Dataset/masks/train/' + annotationNum + '.png'
         img.save(mask)
         continue
     else:
         continue
+    
+
+# for file in os.scandir(annotationDir):
+#     filename = os.fsdecode(file)
+#     if filename.endswith(".xml"):
+#         annotationNum, boxes, width, height = extract_info(annotationDir + imageNum +'.xml')
+#         # creating new Image object
+#         img = Image.new('RGB',(width,height),0)
+#         img1 = ImageDraw.Draw(img)
+#         for b in range(len(boxes)):
+#         # create rectangle image
+#             img1.rectangle((boxes[b]), fill ='white',outline='white')
+#         # img.show()
+#         mask = '../Dataset/masks/' + annotationNum + '.png'
+#         img.save(mask)
+#         continue
+#     else:
+#         continue
